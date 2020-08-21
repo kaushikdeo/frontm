7b87266bb465d71575d9732d661a0ba1294cbfb9
Sunil <suniljoshi.is.available@gmail.com>
2020-08-21 09:50:20 +0530

    Update transactions.js

--- transactions.js
+++ transactions.js
@@ -1,12 +1,13 @@
+// code-review - create two repos if there are two assignments
 async function addOrder(client, userEmail, nameOfListing, reservationDates, reservationDetails) {
- 
+   // code-review - take all the above inputs in one object. Its more scalable and will not change the function defination
     const usersCollection = client.db("sample_airbnb").collection("users");
     const listingsAndReviewsCollection = client.db("sample_airbnb").collection("listingsAndReviews");
- 
+ // code-review - why the addOrder function will have the createReservationDocument function
     const reservation = createReservationDocument(nameOfListing, reservationDates, reservationDetails);
- 
+ // code-review - Session creation and transaction code will be part of helper function which can be reused by other functions
     const session = client.startSession();
- 
+
     const transactionOptions = {
         readPreference: 'primary',
         readConcern: { level: 'local' },
@@ -14,12 +15,14 @@ async function addOrder(client, userEmail, nameOfListing, reservationDates, rese
     };
  
     try {
+     // code-review - helper here too, addOrder should only contain its business logic
         const transactionResults = await session.withTransaction(async () => {
  
             const usersUpdateResults = await usersCollection.updateOne(
                 { email: userEmail },
                 { $addToSet: { reservations: reservation } },
                 { session });
+         // code-review - have a logger helper which does the logging Logger.info(...) and that info function will do console.log
             console.log(`${usersUpdateResults.matchedCount} document(s) found in the users collection with the email address ${userEmail}.`);
             console.log(`${usersUpdateResults.modifiedCount} document(s) was/were updated to include the reservation.`);
  
@@ -31,6 +34,7 @@ async function addOrder(client, userEmail, nameOfListing, reservationDates, rese
                 await session.abortTransaction();
                 console.error("This listing is already reserved for at least one of the given dates. The reservation could not be created.");
                 console.error("Any operations that already occurred as part of this transaction will be rolled back.");
+             // code-review - throw error here and abort transaction in catch
                 return;
             }
  
@@ -49,6 +53,7 @@ async function addOrder(client, userEmail, nameOfListing, reservationDates, rese
             console.log("The transaction was intentionally aborted.");
         }
     } catch(e){
+     // code-review - throw error here 
         console.log("The transaction was aborted due to an unexpected error: " + e);
     } finally {
         await session.endSession();
